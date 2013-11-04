d3fa6d1e9be89264b8b73f07da202611253cd76b
Cody Duncan <codyduncan1260@live.com>
2013-11-03 18:10:30 -0800

    Removed TestClass

--- ShadyTreeTest/Driver.cpp
+++ ShadyTreeTest/Driver.cpp
@@ -2,13 +2,11 @@
 #include <stdlib.h>
 #include <string.h>
 #include <tchar.h>
-#include "TestClass.h"
 #include "MathLib.h"
 
 int WINAPI WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd )
 {
     Vector4 h;
 
-    TestClass c;
     return 0;
 }
