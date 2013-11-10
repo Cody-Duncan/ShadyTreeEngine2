af53b64dbca3081a30e321bbec7be603c5f7191b
Cody Duncan <codyduncan1260@live.com>
2013-11-09 17:24:57 -0800

    Working on refactoring the way the project copies dlls to the output directory.

--- build_scripts/test_post_build.bat
+++ build_scripts/test_post_build.bat
@@ -1,8 +1,8 @@
 setlocal EnableDelayedExpansion
 
-set TargetDir=%1
-set SolutionDir=%2
-set Debug=%3
+set TargetDir=%~1
+set SolutionDir=%~2
+set Debug=%~3
 
 echo.
 echo.
@@ -12,23 +12,18 @@ xcopy "%SolutionDir%resources\*" "%TargetDir%resources\" /F /Y /S
 echo Resources Complete
 echo.
 
-if "%Debug%" EQU "-d" ( 
-	for /r %SolutionDir%\lib %%f in (*D.dll) do copy %%f %TargetDir% /Y
-	echo DLL's Complete
-	echo.
+echo "%SolutionDir%lib\ShadyTreeEngineD\*.dll"
+
+if "%Debug%" EQU "-d" (
 	
-	echo Copying PDB files
-	for /r %SolutionDir%\lib %%f in (*.pdb) do copy %%f %TargetDir% /Y
-	echo.
+	xcopy %SolutionDir%lib\ShadyTreeEngineD\*.dll "%TargetDir%" /F /Y /S
+	xcopy "%SolutionDir%lib\ShadyTreeEngineD\*.pdb" "%TargetDir%" /F /Y /S
+	xcopy "%SolutionDir%lib\ShadyTreeEngineD\*.lib" "%TargetDir%" /F /Y /S
+ 
 ) else (
-	for /r %SolutionDir%\lib %%f in (*.dll) do (
-		set "var=%%f"
-		if "!var:~-5!" NEQ "D.dll" ( 
-			echo copying: %%~nf%%~xf
-			copy %%f %TargetDir% /Y )
-	)
-	echo DLL's Complete
-	echo.
+	xcopy "%SolutionDir%lib\ShadyTreeEngine\*.dll" "%TargetDir%" /F /Y /S
+	xcopy "%SolutionDir%lib\ShadyTreeEngine\*.pdb" "%TargetDir%" /F /Y /S
+	xcopy "%SolutionDir%lib\ShadyTreeEngine\*.lib" "%TargetDir%" /F /Y /S
 )
 echo PostBuild copy complete
 echo.
